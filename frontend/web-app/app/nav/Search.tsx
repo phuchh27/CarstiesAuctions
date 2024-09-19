1cc108beedee7c28b915b50bd0dd384c91c7b337
phuchh27 <phuchh27@gmail.com>
2024-09-19 14:47:10 +0700

    add return home page when search

--- frontend/web-app/app/nav/Search.tsx
+++ frontend/web-app/app/nav/Search.tsx
@@ -1,9 +1,12 @@
 "use client";
 import { useParamsStore } from "@/hooks/useParamsStore";
+import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 
 export default function Search() {
+  const router = useRouter();
+  const pathname = usePathname();
   const setParams = useParamsStore((state) => state.setParams);
   const setSearchvalue = useParamsStore((state) => state.setSearchvalue);
   const searchValue = useParamsStore((state) => state.searchValue);
@@ -12,6 +15,7 @@ export default function Search() {
   }
 
   function search() {
+    if (pathname !== "/") router.push("/");
     setParams({ searchTerm: searchValue });
   }
 
